171224642c55f0618201be41a4676f2008b164a7
efecantaslak <efecan_taslak@hotmail.com>
2023-08-08 23:19:11 +0300

    Pages are registered.

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,11 +6,35 @@ const router = createRouter({
     {
       path: '/',
       component: () => import('../layouts/MainLayout.vue'),
+      redirect: 'home',
       children: [{
-        path: '',
+        path: 'home',
+        name: 'Home',
         component: () => import('../views/HomeView.vue'),
+      }, {
+        path: 'reports',
+        name: 'Reports',
+        component: () => import('../views/ReportsView.vue'),
+      }, {
+        path: 'insights',
+        name: 'Insights',
+        component: () => import('../views/InsightsView.vue'),
+      }, {
+        path: 'actions',
+        name: 'Actions',
+        component: () => import('../views/ActionsView.vue'),
       }]
     },
+    {
+      path: '/auth',
+      component: () => import('../layouts/AuthLayout.vue'),
+      redirect: { name: 'Login' },
+      children: [{
+        path: 'login',
+        name: 'Login',
+        component: () => import('../views/LoginView.vue'),
+      }],
+    }
   ]
 })
 
