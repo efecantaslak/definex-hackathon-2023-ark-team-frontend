4cceb8ba4a3e2fccd7914029c2cdc74c57f6dfe9
efecantaslak <efecan_taslak@hotmail.com>
2023-08-08 23:13:56 +0300

    Unauthorized layout created.

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout>
+    <v-app-bar title="PredictX">
+      <template v-slot:prepend>
+        <v-icon icon="diamond"></v-icon>
+      </template>
+    </v-app-bar>
+    <v-main class="ma-2">
+      <router-view></router-view>
+    </v-main>
+  </v-layout>
+</template>
+<script setup lang="ts">
+</script>
